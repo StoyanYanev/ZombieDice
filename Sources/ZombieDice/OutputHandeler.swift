cab3c7640ac5208d3b4c4de5e71a6acaaf1c6b58
Stoyan Yanev <syanev@vmware.com>
2020-07-12 16:04:38 +0300

    Add new methods in OutputHandeler

--- Sources/ZombieDice/OutputHandeler.swift
+++ Sources/ZombieDice/OutputHandeler.swift
@@ -1,21 +1,34 @@
 import SwiftyTextTable
 
-final class OutputHandeler {
+public class OutputHandeler {
     let TABLE_HEADER = "Players"
+    let TABLE_WINNER_HEADER = "WINER"
     
-    private var players: [Player]
+    init(){}
     
-    init(players: [Player]){
-        self.players = players
-    }
-    
-    func drawResult(){
-        let playerNames = self.players.map{$0.getName()}.map{TextTableColumn(header: $0)}
-        let playerPoints = self.players.map{$0.getPoints()}
+    func drawResult(players: [Player]){
+        let playerNames = players.map{$0.getName()}.map{TextTableColumn(header: $0)}
+        let playerPoints = players.map{$0.getBrainScore()}
         
         var table = TextTable(columns: playerNames)
         table.header = TABLE_HEADER
         table.addRow(values: playerPoints)
+        
+        print(table.render())
+    }
+    
+    func printPlayerTurn(player: Player) {
+        print("\(player.getName()) it is your turn")
+    }
+    
+    func printPlayerEndGame(player: Player) {
+        print("\(player.getName()) you lost")
+    }
+    
+    func printWinner(player: Player) {
+        var table = TextTable(columns: ["Name", "Points"].map{TextTableColumn(header: $0)})
+        table.header = TABLE_WINNER_HEADER
+        table.addRow(values: [player.getName(), player.getBrainScore()])
         
         print(table.render())
     }
